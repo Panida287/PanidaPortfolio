3c2736acb0b982b1c86ffdb1cdd857dcab3fcafd
Panida Paethanom <143201084+Panida287@users.noreply.github.com>
2025-05-29 21:38:28 +0200

    feat: added pages and layout

---
+++ src/main.tsx
@@ -0,0 +1,42 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './css/style.css';
+import App from './App.tsx';
+import ProjectsPage from './pages/ProjectsPage';
+import ProjectDetailPage from './pages/ProjectDetailPage';
+import Layout from './Layout';
+import NotFoundPage from './pages/NotFoundPage';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Layout />,
+		children: [
+			{
+				path: '',
+				element: <App />
+			},
+			{
+				path: 'projects',
+				element: <ProjectsPage />
+			},
+			{
+				path: 'projects/:id',
+				element: <ProjectDetailPage />
+			}
+		]
+	},
+	{
+		path: '*',
+		element: <NotFoundPage />,
+	}
+]);
+
+createRoot(document.getElementById('root')!).render(
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>
+);
+
+
