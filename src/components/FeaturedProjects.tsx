7244cb0a2b01f9a80e6848f1ef4bf73430107e15
Panida Paethanom <143201084+Panida287@users.noreply.github.com>
2025-06-09 19:15:00 +0200

    feat: added view more project btn

--- src/components/FeaturedProjects.tsx
+++ src/components/FeaturedProjects.tsx
@@ -1,5 +1,6 @@
-import ShuffleCarousel, { CarouselItem } from "./ShuffleCarousel";
+import ShuffleCarousel, { CarouselItem } from './ShuffleCarousel';
 import rawProjects from '../../public/data/projects.json';
+import { Link } from 'react-router-dom';
 
 export default function FeaturedProjects() {
 	const items: CarouselItem[] = rawProjects.map((p) => ({
@@ -14,17 +15,24 @@ export default function FeaturedProjects() {
 	return (
 		<section>
 			<div className="my-10 flex flex-col gap-2 justify-center items-start border-neon/20 border rounded-xl p-4">
-			<h2 className="font-heading text-2xl flex gap-2">
-				<span className="text-white">Featured</span>
-				<span className="text-pink">Projects</span>
-			</h2>
-			<p className="font-paragraph text-xs font-thin">
-				Some of my favorite projects.
-			</p>
+				<h2 className="font-heading text-2xl flex gap-2">
+					<span className="text-white">Featured</span>
+					<span className="text-pink">Projects</span>
+				</h2>
+				<p className="font-paragraph text-xs font-thin">
+					Some of my favorite projects.
+				</p>
 
 			</div>
 
 			<ShuffleCarousel items={items} />
+			<span className="button-container flex justify-end items-center my-5">
+            <Link to="/projects"
+                  className="button mt-2">
+            View more projects
+	            <i className="fa-duotone fa-regular fa-angle-right pl-4"></i>
+            </Link>
+        </span>
 		</section>
 	);
 }
